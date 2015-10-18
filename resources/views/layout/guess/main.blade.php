4c365e9154b1e9ccf624ce3ab8145548953c1769
ilfate <ilfate@gmail.com>
2015-10-19 00:06:32 +0200

    GuessSeries changed a little.

--- resources/views/layout/guess/main.blade.php
+++ resources/views/layout/guess/main.blade.php
@@ -4,6 +4,9 @@
 
 
 <div class="guess-body">
+    <div class="top-navigation">
+        <a href="{{ @action('GamesController@index') }}">Back to games</a>
+    </div>
     <div class="container main">
         <div class="row">
             <div class="col-md-9 game-area">
