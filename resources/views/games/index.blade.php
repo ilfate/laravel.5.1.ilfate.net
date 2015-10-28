cd3754f203d530b5db0b7ef3f32992d5419de199
ilfate <ilfate@gmail.com>
2015-10-29 00:16:14 +0100

    Redesign. Commit for deploy test

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -2,23 +2,6 @@
 
 @section('content')
 
-    {{--<div class="games-page">--}}
-        {{--<div class="block-container">--}}
-            {{--<div class="series game-block">--}}
-                {{--<a href="{{ action('GuessGameController@index') }}">--}}
-                    {{--<img src="/images/game/GuessSeriesLowQuality.jpg" />--}}
-                {{--</a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="block-container">--}}
-            {{--<div class="math-effect game-block">--}}
-                {{--<a href="{{ action('MathEffectController@index') }}">--}}
-                    {{--<img src="/images/game/tdTitle-1011.jpg" />--}}
-                {{--</a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
 <div class="games-page">
     <div class="landing-block guess-series">
         <a href="{{ action('GuessGameController@index') }}" class="rounded_block_link" data-target=".main-content-well">
@@ -28,6 +11,7 @@
     </div>
     <div class="landing-block math-effect">
         <a href="{{ action('MathEffectController@index') }}" class="rounded_block_link" data-target=".main-content-well">
+            <i class="fa fa-th-large opacity-animation"></i>
             <span class="text">Math Effect</span>
         </a>
     </div>
