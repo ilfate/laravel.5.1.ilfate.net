2578ff88a45f40a58585de17d9ac4e016c55d010
ilfate <ilfate@gmail.com>
2018-05-23 08:04:41 +0200

    remove from gdpr

--- resources/views/blocks/gdpr.blade.php
+++ resources/views/blocks/gdpr.blade.php
@@ -4,5 +4,3 @@
     <a target="_blank" href="/disclaimer.html">Disclaimer</a>
     <a target="_blank" href="/cookies-policy.html">Cookies policy</a>
 </div>
-
-<script id="CookieDeclaration" src="https://consent.cookiebot.com/b7f96168-f47c-40a4-83fb-1693d5dd9da3/cd.js" type="text/javascript" async></script>
