256dafd18a131df27ebba1320dc984f279697f2d
ilfate <ilfate@gmail.com>
2021-02-16 21:00:42 +0100

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,14 +7,14 @@
 {{--</div>--}}
 
 <div class="landing-page-container">
-    <div class="main-page-block cv">
-        <a href="{{ action('PageController@cv') }}" class="rounded_block_link">
-            <div class="rounded_block" >
-                <span class="text">Cv</span>
+   {{--   <div class="main-page-block cv"> --}}
+    {{--      <a href="{{ action('PageController@cv') }}" class="rounded_block_link"> --}}
+      {{--        <div class="rounded_block" > --}}
+      {{--            <span class="text">Cv</span> --}}
     {{--         @include('interface.button-block', array('text' => 'CV', 'background' => '/images/my/code1_s.jpg'))--}}
-            </div>
-        </a>
-    </div>
+    {{--          </div> --}}
+    {{--      </a> --}}
+   {{--   </div> --}}
     <div class="main-page-block code">
         <a href="{{ action('CodeController@index') }}" class="rounded_block_link">
             <div class="rounded_block" >
