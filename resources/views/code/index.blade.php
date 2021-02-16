a8245ce96804a0f473167fbc48aeaba785dac8e2
ilfate <ilfate@gmail.com>
2021-02-16 21:12:00 +0100

    Update index.blade.php

--- resources/views/code/index.blade.php
+++ resources/views/code/index.blade.php
@@ -4,12 +4,12 @@
 
 <div class="code-page">
 
-    {-- <div class="landing-block github"> --}
-    {--     <a target="_blank" href="https://github.com/ilfate/laravel.5.1.ilfate.net" class="rounded_block_link" data-target=".main-content-well"> --}
-       {--      <img src="/svg/github.svg" class="opacity-animation" /> --}
-          {--   <span class="text bounce">Github</span> --}
-       {--  </a> --}
-    {-- </div> --}
+    {{-- <div class="landing-block github"> --}}
+    {{--     <a target="_blank" href="https://github.com/ilfate/laravel.5.1.ilfate.net" class="rounded_block_link" data-target=".main-content-well"> --}}
+       {{--      <img src="/svg/github.svg" class="opacity-animation" /> --}}
+          {{--   <span class="text bounce">Github</span> --}}
+       {{--  </a> --}}
+    {{-- </div> --}}
     <div class="landing-block game-template">
         <a href="{{ action('CodeController@gameTemplate') }}" class="rounded_block_link" data-target=".main-content-well">
             <span class="text bounce">Game-template</span>
